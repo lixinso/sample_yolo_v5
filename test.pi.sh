9fe58c28da163d505182b11a9e0daeec19420d8c
Xinsong Li <lixinso@gmail.com>
2020-06-13 12:41:51 -0500

    try run yolov5

--- test.pi.sh
+++ test.pi.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/ultralytics/yolov5.git
 pip3 install -U -r requirements.txt #remove opencv-python before run it
+python3 detect.py --source ./inference/images/ --weights yolov5s.pt --conf 0.4 # Failed
