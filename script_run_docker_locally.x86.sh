0764e7bb289c55737516df9d8a1f3a8d533258be
Xinsong Li <lixinso@gmail.com>
2020-06-21 13:12:18 -0500

    fix Dockerfile

--- script_run_docker_locally.x86.sh
+++ script_run_docker_locally.x86.sh
@@ -1,2 +1,2 @@
 sudo docker build -t yolov5_x86 . -f Dockerfile.x86
-sudo docker run -it --rm  --network host -v `pwd`:/myworkdir -v /data2:/data2 -v  /etc yolov5_x86
+sudo docker run -it --rm  --network host -v `pwd`:/myworkdir -v  /etc yolov5_x86
